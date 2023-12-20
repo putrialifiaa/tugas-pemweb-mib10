0f5e1a39e4ee94d79bc18ee36c593eb2af52ace2
Daniantoo <dion.22058@mhs.unesa.ac.id>
2023-12-20 12:52:00 +0700

    memindahkan file
    memindahkan file ken tiap folder agar tertata rapi

---
+++ database/register.php
@@ -0,0 +1,25 @@
+<?php
+// Koneksi ke database
+$db = new mysqli("localhost", "username", "password", "mydatabase");
+
+if ($db->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+
+// Tangkap data dari formulir
+$username = $_POST['username'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+// Masukkan data ke database
+$query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+$result = $db->query($query);
+
+if ($result) {
+    echo "Registration successful. <a href='login.html'>Login</a>";
+} else {
+    echo "Registration failed. Please try again.";
+}
+
+// Tutup koneksi database
+$db->close();
+?>
